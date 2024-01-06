f104f37afa044cefd5ca8edd1e3c2efafdd3ea90
Amrit Sundarka <amritsundarka@gmail.com>
2024-01-06 17:35:34 +0530

    Working Reply

--- ChatBot/ChatApp/static/js/script.js
+++ ChatBot/ChatApp/static/js/script.js
@@ -1,6 +1,6 @@
 function sendMessage() {
     var message = document.getElementById('message').value;
-    fetch('/myapp/get_next_char/' + message + '/')
+    fetch('/get_next_char/' + message + '/')
         .then(response => response.json())
         .then(data => {
             document.getElementById('response').innerText = 'Next Character: ' + data.next_char;
--- ChatBot/ChatApp/views.py
+++ ChatBot/ChatApp/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'ChatApp/index.html')
+    return render(request, 'index.html')
 
 def get_next_char(request, message):
     if message.isalpha() and len(message) == 1:
