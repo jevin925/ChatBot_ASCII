bdca15716c13b56483652d4997571b6b2a806162
Amrit Sundarka <amritsundarka@gmail.com>
2024-01-06 17:31:34 +0530

    Added Render

--- ChatBot/ChatApp/urls.py
+++ ChatBot/ChatApp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import get_next_char
+from . import views
 
 urlpatterns = [
+    path('',views.index,name='index'),
     path('get_next_char/<str:message>/', get_next_char, name='get_next_char'),
+    
 ]
--- ChatBot/ChatApp/views.py
+++ ChatBot/ChatApp/views.py
@@ -1,4 +1,8 @@
 from django.http import JsonResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'ChatApp/index.html')
 
 def get_next_char(request, message):
     if message.isalpha() and len(message) == 1:
